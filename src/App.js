690d946992c52d93e960660b8cee4650b3c429af
thulio1603 <thulio1603@gmail.com>
2022-07-09 18:42:41 -0300

    estudo Nextui

---
+++ src/App.js
@@ -0,0 +1,31 @@
+// 1. import `NextUIProvider` component
+import { NextUIProvider } from "@nextui-org/react";
+import "./App.css";
+import { Suspense, lazy } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Header";
+
+const Product = lazy(() => import("./components/Product"));
+const Cart = lazy(() => import("./components/Cart"));
+
+function App() {
+  // 2. Use at the root of your app
+  return (
+    <NextUIProvider>
+      <Navbar />
+      <Router>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Product />}>
+              Product
+            </Route>
+            <Route path="/cart" element={<Cart />}>
+              Cart
+            </Route>
+          </Routes>
+        </Suspense>
+      </Router>
+    </NextUIProvider>
+  );
+}
+export default App;
---
+++ src/components/Product.jsx
@@ -0,0 +1,16 @@
+import { Grid, Container, Pagination } from "@nextui-org/react";
+import { products } from "./data";
+import ProductList from "./ProductList";
+
+export default function Product() {
+  return (
+    <Container>
+      <Grid.Container gap={2} justify="center">
+        {products.map((product) => (
+          <ProductList key={product.id} product={product} />
+        ))}
+      </Grid.Container>
+      <Pagination total={20} initialPage={1} />;
+    </Container>
+  );
+}
